875a52ba401d48eab07f6ee7163d9a282f237f58
SofiaSerrano2 <98984365+SofiaSerrano2@users.noreply.github.com>
2022-07-14 16:04:20 -0300

    Disenio buscador terminado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+//css
+import "./img.scss";
+import "./body.scss";
+//imagenes
+import logo from "./img/people-logo.png";
+import lupa from "./img/vidrio-de-aumento.png";
+
 export default function App() {
   return (
     <div className="App">
-      <h1>hola</h1>
+      <div className="buscador_logos">
+        <h1 className="nombre_pag">Buscador de personas</h1>
+        <img src={logo} className="logo_img" />
+      </div>
+      <div className="buscador_inputs">
+        <input type="text" className="input" />
+        <img src={lupa} className="lupa_img" />
+      </div>
     </div>
   );
 }
