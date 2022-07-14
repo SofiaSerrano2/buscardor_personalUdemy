f3f7fe1519ce25d1fa69fec418276df8f6f56cbf
SofiaSerrano2 <98984365+SofiaSerrano2@users.noreply.github.com>
2022-07-14 14:11:20 -0300

    Cambios Primera subida

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "../src/componentes/Input/Input.scss";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>primera oracion</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
